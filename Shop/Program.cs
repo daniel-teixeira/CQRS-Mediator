716a555e21aa3481bff6f06d9bdc752b00cf446d
Daniel Teixeira <daniell_augustto@hotmail.com>
2022-01-31 22:29:08 -0300

    CQRS + Mediator Basic

---
+++ Shop/Program.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using MediatR;
+using Shop.Domain.Commands.Handlers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Mediator Dependencia
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
+//Injeção de dependencia
+//builder.Services.AddTransient<ICreateCustomerHandler, CreateCustomerHandler>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
